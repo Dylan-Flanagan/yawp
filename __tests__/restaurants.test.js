0f20d023393e0800b7ed05c00f4f7b67f32edbd4
Dylan <dylflanagan@gmail.com>
2022-11-28 16:15:57 -0800

    Test returning list of restaurants passed

--- __tests__/restaurants.test.js
+++ __tests__/restaurants.test.js
@@ -1,5 +1,5 @@
-// const pool = require('../lib/utils/pool.js');
-// const setup = require('../data/setup.js');
+const pool = require('../lib/utils/pool.js');
+const setup = require('../data/setup.js');
 const request = require('supertest');
 const app = require('../lib/app');
 // const UserService = require('../lib/services/UserService.js');
@@ -13,38 +13,52 @@ const app = require('../lib/app');
 //   password: '12345',
 // };
 
-it('GET api/v1/restaurants should return a list of restaurants', async () => {
-  const resp = await request(app).get('/api/v1/restaurants');
-  expect(resp.status).toBe(200);
-  expect(resp.body).toMatchInlineSnapshot(`
-  Array [
-    Object {
-        "name": "Pip''s Original",
-        "cuisine": "American",
-        "cost": "1",
-        "image": "https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg",
-        "website": "http://www.PipsOriginal.com",
-    },
-    Object {
-        "name": "Mucca Osteria",
-        "cuisine": "Italian",
-        "cost": "3",
-        "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/13/af/df/89/duck.jpg",
-        "website": "http://www.muccaosteria.com",
-    },
-    Object {
-        "name": "Mediterranean Exploration Company",
-        "cuisine": "Mediterranean",
-        "cost": "2",
-        "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/1c/f2/e5/0c/dinner.jpg",
-        "website": "http://www.mediterraneanexplorationcompany.com/",
-    },
-    Object {
-        "name": "Salt & Straw",
-        "cuisine": "American",
-        "cost": "2",
-        "image": "https://media-cdn.tripadvisor.com/media/photo-o/0d/d6/a1/06/chocolate-gooey-brownie.jpg",
-        "website": "https://saltandstraw.com/pages/nw-23",
-    },
-  ]`);
+describe('restaurant routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+  it('GET api/v1/restaurants should return a list of restaurants', async () => {
+    const resp = await request(app).get('/api/v1/restaurants');
+    expect(resp.status).toBe(200);
+    expect(resp.body).toMatchInlineSnapshot(`
+          Array [
+            Object {
+              "cost": 1,
+              "cuisine": "American",
+              "id": "1",
+              "image": "https://media-cdn.tripadvisor.com/media/photo-o/05/dd/53/67/an-assortment-of-donuts.jpg",
+              "name": "Pip's Original",
+              "website": "http://www.PipsOriginal.com",
+            },
+            Object {
+              "cost": 3,
+              "cuisine": "Italian",
+              "id": "2",
+              "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/13/af/df/89/duck.jpg",
+              "name": "Mucca Osteria",
+              "website": "http://www.muccaosteria.com",
+            },
+            Object {
+              "cost": 2,
+              "cuisine": "Mediterranean",
+              "id": "3",
+              "image": "https://media-cdn.tripadvisor.com/media/photo-m/1280/1c/f2/e5/0c/dinner.jpg",
+              "name": "Mediterranean Exploration Company",
+              "website": "http://www.mediterraneanexplorationcompany.com/",
+            },
+            Object {
+              "cost": 2,
+              "cuisine": "American",
+              "id": "4",
+              "image": "https://media-cdn.tripadvisor.com/media/photo-o/0d/d6/a1/06/chocolate-gooey-brownie.jpg",
+              "name": "Salt & Straw",
+              "website": "https://saltandstraw.com/pages/nw-23",
+            },
+          ]
+        `);
+  });
+
+  afterAll(() => {
+    pool.end();
+  });
 });
